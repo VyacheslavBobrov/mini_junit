4a46abed0a1c30666a2948f2d17c971cf04a1ef5
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2020-02-05 17:04:10 +0300

    Добавил получение списка классов из аргументов приложения

--- src/main/java/ru/bobrov/vyacheslav/minijunit/Runner.java
+++ src/main/java/ru/bobrov/vyacheslav/minijunit/Runner.java
@@ -1,22 +1,43 @@
 package ru.bobrov.vyacheslav.minijunit;
 
 import ru.bobrov.vyacheslav.minijunit.annotations.Test;
-import ru.bobrov.vyacheslav.minijunit.tests.TestRun;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Runner {
     public static void main(String[] args) {
-        (new Runner()).run(new Class[]{TestRun.class});
+        if (args.length == 0 || args[0].equals("-h") || args[0].equals("--help")) {
+            System.out.println("runner package.Class1 package.Class2 ... package.ClassN");
+            return;
+        }
+
+        Runner runner = new Runner();
+        runner.run(
+                Arrays.stream(args)
+                        .map(className -> {
+                            try {
+                                return Class.forName(className);
+                            } catch (ClassNotFoundException e) {
+                                e.printStackTrace();
+                                return null;
+                            }
+                        })
+                        .filter(Objects::nonNull)
+                        .toArray(Class<?>[]::new)
+        );
     }
 
     public void run(Class<?>... classes) {
         Arrays.stream(classes).forEach(clazz -> {
             try {
                 run(clazz.getConstructor().newInstance());
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            } catch (InstantiationException
+                    | IllegalAccessException
+                    | InvocationTargetException
+                    | NoSuchMethodException e) {
                 e.printStackTrace();
             }
         });
